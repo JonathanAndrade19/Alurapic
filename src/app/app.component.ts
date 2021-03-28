c635232be3237925b07ee6792c22314267dd494c
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-03-28 16:14:36 -0300

    new inplements ngfor in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  photos = [
+    {
+      url: '../assets/img/Gabi.jpg',
+      description: 'Gabi and Gerson'
+    },
+    {
+      url: '../assets/img/flamengo-2020.jpg',
+      description: 'team of Flamengo'
+    },
+    {
+      url: '../assets/img/campeao.jpeg',
+      description: 'congratulation'
+    }
+  ]
+
 }
