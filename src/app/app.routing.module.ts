6698198c1c32d4fe170a7c5680224c68181d1759
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-04-03 21:16:23 -0300

    new page errors.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoNameComponent } from './photos/photo-name/photo-name.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
     { path: 'user/flavio', component: PhotoListComponent },
     { path: 'p/add', component: PhotoFormComponent },
-    { path: 'name', component: PhotoNameComponent }
+    { path: 'name', component: PhotoNameComponent },
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
